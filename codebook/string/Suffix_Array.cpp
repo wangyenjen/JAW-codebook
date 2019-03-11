7502cef14234f819c867b04219c194796bdecd2e
wangyenjen <b06902093@ntu.edu.tw>
2019-03-11 12:19:25 +0800

    update 2019/3/11 12:19

--- codebook/string/Suffix_Array.cpp
+++ codebook/string/Suffix_Array.cpp
@@ -1,44 +1,65 @@
-const int MAX = 1020304;
-int ct[MAX], he[MAX], rk[MAX];
-int sa[MAX], tsa[MAX], tp[MAX][2];
-void suffix_array(char *ip){
-	int len = strlen(ip);
-	int alp = 256;
-	memset(ct, 0, sizeof(ct));
-	for(int i=0;i<len;i++) ct[ip[i]+1]++;
-	for(int i=1;i<alp;i++) ct[i]+=ct[i-1];
-	for(int i=0;i<len;i++) rk[i]=ct[ip[i]];
-	for(int i=1;i<len;i*=2){
-		for(int j=0;j<len;j++){
-			if(j+i>=len) tp[j][1]=0;
-			else tp[j][1]=rk[j+i]+1;		
-			tp[j][0]=rk[j];
-		}
-		memset(ct, 0, sizeof(ct));
-		for(int j=0;j<len;j++) ct[tp[j][1]+1]++;
-		for(int j=1;j<len+2;j++) ct[j]+=ct[j-1];
-		for(int j=0;j<len;j++) tsa[ct[tp[j][1]]++]=j;
-		memset(ct, 0, sizeof(ct));
-		for(int j=0;j<len;j++) ct[tp[j][0]+1]++;
-		for(int j=1;j<len+1;j++) ct[j]+=ct[j-1];
-		for(int j=0;j<len;j++)
-      sa[ct[tp[tsa[j]][0]]++]=tsa[j];
-		rk[sa[0]]=0;
-		for(int j=1;j<len;j++){
-			if( tp[sa[j]][0] == tp[sa[j-1]][0] &&
-				tp[sa[j]][1] == tp[sa[j-1]][1] )
-				rk[sa[j]] = rk[sa[j-1]];
-			else
-				rk[sa[j]] = j;
-		}
-	}
-	for(int i=0,h=0;i<len;i++){
-		if(rk[i]==0) h=0;
-		else{
-			int j=sa[rk[i]-1];
-			h=max(0,h-1);
-			for(;ip[i+h]==ip[j+h];h++);
-		}
-		he[rk[i]]=h;
-	}
-}
+/**
+ * O(n log n log n) suffix array.
+ *
+ * Use the commented parts for O(n log n) version.
+ *
+ * Interface:
+ * + Let `s` be a null-terminated string of length `n`
+ * + After calling `(*this)(s)`...
+ * + `sa[0:n]` contains the suffix array
+ * + `r[sa[i]] = i` for 0 <= i < n
+ * + `h[i] = LCP(s+sa[i], s+sa[i+1])` for 0 <= i < n - 1
+ * + `h[n - 1] = 0`
+ */
+template<int maxn>
+struct Suffix_array {
+    int sa[maxn], r[maxn], h[maxn], tmp[maxn], cnt[maxn];
+
+    /* void counting_sort(int n) {
+        fill_n(cnt, max(n, 256), 0);
+        for (int i = 0; i < n; i++)
+            cnt[r[i]]++;
+        partial_sum(cnt, cnt + max(n, 256), cnt);
+        for (int i = n - 1; i >= 0; i--)
+            tmp[--cnt[r[sa[i]]]] = sa[i];
+        copy_n(tmp, n, sa);
+    } */
+
+    void operator () (const char *s) {
+        int n = strlen(s);
+        if (n == 1) { sa[0] = r[0] = 0; return; }
+
+        copy_n(s, n, r);
+        iota(sa, sa + n, 0);
+        /* counting_sort(n); */
+
+        for (int g = 1; g < n; g <<= 1) {
+            auto cmp = [=](int a, int b) {
+                return r[a] != r[b] ? r[a] < r[b] :
+                    (a + g >= n || b + g >= n) ? a > b :
+                    r[a + g] < r[b + g];
+            };
+
+            sort(sa, sa + n, cmp);
+            /* for (int i = n - 1, j = n - 1; i >= 0; i--)
+                if (sa[i] >= g)
+                    sa[j--] = sa[i] - g;
+            iota(sa, sa + g, n - g);
+            counting_sort(n); */
+
+            tmp[sa[0]] = 0;
+            for (int i = 1; i < n; i++)
+                tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+            copy_n(tmp, n, r);
+        }
+
+        h[n - 1] = 0;
+        for (int i = 0; i < n; i++)
+            if (r[i] + 1 < n) {
+                int x = i ? max(h[r[i - 1]] - 1, 0) : 0;
+                while (s[i + x] == s[sa[r[i] + 1] + x])
+                    x++;
+                h[r[i]] = x;
+            }
+    }
+};
--- codebook/string/zvalue_palindrome.cpp
+++ codebook/string/zvalue_palindrome.cpp
@@ -1,29 +1,34 @@
-int len, zv[MAX*2];
-char ip[MAX], op[MAX*2];
-int main(){
-	cin >> ip; len = strlen(ip);
-	int l2 = len*2 - 1;
-	for(int i=0; i<l2; i++)
-		if(i&1) op[i] = '@';
-		else op[i] = ip[i/2];
-	int l=0, r=0; zv[0] = 1;
-	for(int i=1; i<l2; i++){
-		if( i > r ){
-			l = r = i;
-			while( l>0 && r<l2-1 && op[l-1] == op[r+1] )
-				l --, r ++;
-			zv[i] = (r-l+1);
-		}else{
-			int md = (l+r)/2, j = md + md - i;
-			zv[i] = zv[j];
-			int q = zv[i] / 2, nr = i + q;
-			if( nr == r ){
-				l = i + i - r;
-				while( l>0 && r<l2-1 && op[l-1] == op[r+1] )
-					l --, r ++;
-				zv[i] = r - l + 1;
-			}else if( nr > r )
-				zv[i] = (r - i) * 2 + 1;
-		}
-	}
+/**
+ * s[i - z[i] + 1 : i + z[i]] is palindrome.
+ */
+void manacher(char *s, int *z, int n) {
+    z[0] = 1;
+    for (int i = 1, l = 0, r = 1; i < n; i++) {
+        int x = i < r ? min(r - i, z[2 * l - i]) : 1;
+        while (i - x >= 0 && i + x < n && s[i - x] == s[i + x])
+            x++;
+        z[i] = x;
+        if (i + x > r)
+            l = i, r = i + x;
+    }
+}
+
+char s[3001], s2[6002];
+int z[6001];
+
+int main() {
+    scanf("%3000s", s);
+    int n = strlen(s);
+
+    for (int i = 0; i < n; i++)
+        s2[2 * i] = '.', s2[2 * i + 1] = s[i];
+    s2[2 * n] = '.';
+
+    int m = 2 * n + 1;
+    s2[m] = '\n';
+
+    manacher(s2, z, m);
+    int maxi = *max_element(z, z + m);
+    int cnt = count(z, z + m, maxi);
+    printf("%d %d\n", maxi - 1, cnt);
 }
